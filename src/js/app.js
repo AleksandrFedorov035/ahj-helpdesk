98b2011f202bc7932a516e7571a744f9ea382a46
a.fedorov035 <aleksandrnosov2008@gmail.com>
2025-07-08 14:05:38 +0300

    add class ticket for api

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const container = document.querySelector(".container");
   new HelpDesk(container);
 });
+
+
+// import Ticket from "../components/Ticket";
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const container = document.querySelector(".container");
+//   new Ticket(container);
+// })
